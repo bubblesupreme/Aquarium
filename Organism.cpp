d048b03ed583ef8b6998c0640c7f34e7112bacdb
Kovalev Vasilii <32467326+KovalevVasilii@users.noreply.github.com>
2018-01-15 18:00:21 +0300

    Add files via upload

---
+++ Organism.cpp
@@ -0,0 +1,29 @@
+#include "Organism.h"
+
+Organism::Organism(coordinates location_, int radOfDisp_, int radOfView_,
+	int lifeTime_, int starvation_) : lifeTime(lifeTime_), radOfDisp(radOfDisp_), radOfView(radOfView_) {};
+
+void Organism::move(coordinates dislocation)
+{
+	location = dislocation;
+};
+
+void Organism::life()
+{
+	lifeTime--;
+}
+
+int Organism::getRadOfView()
+{
+	return radOfView;
+};
+
+int Organism::getRadOfDisp()
+{
+	return radOfDisp;
+};
+
+int Organism::getLifeTime()
+{
+	return lifeTime;
+};
---
+++ aquarium.h
@@ -0,0 +1,36 @@
+#pragma once
+#include "Fish.h"
+#include "Plankton.h"
+#include "Predator.h"
+#include <vector>
+
+///
+class Aquarium
+{
+public:
+	Aquarium(std::pair<int, int> size);
+	Aquarium(std::pair<int, int> size, std::vector<Fish*> listOfFishs,
+		std::vector<Plankton*> listOfPlanktons, std::vector<Predator*> listOfPredators);
+	~Aquarium();
+	void show();
+	void update();
+	void addFish(Fish* fish);
+	void addPlankton(Plankton* plankton);
+	void addPredator(Predator* predator);
+
+
+private:
+	int wave(int x, int y, int exX, int exY, int** map, int n, int m);
+	std::pair<int, int> size;
+	int** map;
+	std::vector<Fish*> listOfFishs;
+	std::vector<Plankton*> listOfPlanktons;
+	std::vector<Predator*> listOfPredators;
+	void reprodaction();
+	void eating();
+	std::map<Organism&, int> searchNeighbors(Fish*);
+	std::map<Organism&, int> searchNeighbors(Plankton*);
+	std::map<Organism&, int> searchNeighbors(Predator*);
+
+};
+
